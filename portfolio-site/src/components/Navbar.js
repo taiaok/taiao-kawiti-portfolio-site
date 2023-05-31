28b788cc6e3fac75a46fba8760a98d24e37ecadd
taiao <taiao.kawiti.wbhs@gmail.com>
2023-05-31 16:59:08 +1200

    Draft link to contact section

--- portfolio-site/src/components/Navbar.js
+++ portfolio-site/src/components/Navbar.js
@@ -5,7 +5,11 @@ export default function NavBar() {
     <div className='navbar'>
       <h3>ABOUT</h3>
       <h3>PROJECTS</h3>
-      <h3>CONTACT</h3>
+      <h3>
+        <a href='#contact' target='_blank' rel='noopener'>
+          CONTACT
+        </a>
+      </h3>
     </div>
   );
 }
