86cc2e0a97356d6d65ff25bb1da24bd3632c9cd1
taiao <taiao.kawiti.wbhs@gmail.com>
2023-06-01 14:13:40 +1200

    Update contact section CSS and content

--- portfolio-site/src/components/Contact.js
+++ portfolio-site/src/components/Contact.js
@@ -1,39 +1,36 @@
+import './Contact.css';
 export default function Contact() {
   return (
     <div className='contact-section' id='contact'>
       <h2>CONTACT</h2>
+      <p>
+        Whether you have a question, a collaboration proposal, or just want to
+        say hello, feel free to reach out to me via email or Linkedin.
+      </p>
       <div className='social-links'>
         <a
           href='https://www.linkedin.com/in/taiaokawiti/'
           target='_blank'
           rel='noreferrer'
         >
-          <p>
-            <i className='fa-brands fa-linkedin'></i>
-          </p>
+          <i className='fa-brands fa-linkedin'></i>
         </a>
         <a href='https://github.com/flowtyf' target='_blank' rel='noreferrer'>
-          <p>
-            <i className='fa-brands fa-github'></i>
-          </p>
+          <i className='fa-brands fa-github'></i>
         </a>
         <a
           href='mailto:taiaokawiti@protonmail.com'
           target='_blank'
           rel='noreferrer'
         >
-          <p>
-            <i className='fa-solid fa-envelope'></i>
-          </p>
+          <i className='fa-solid fa-envelope'></i>
         </a>
         <a
           href='https://drive.google.com/file/d/1va-GCTMhwg_6vAmGu-SP0A97iOQlriCz/view'
           target='_blank'
           rel='noreferrer'
         >
-          <p>
-            <i className='fa-solid fa-file-pdf'></i>
-          </p>
+          <i className='fa-solid fa-file-pdf'></i>
         </a>
       </div>
     </div>
