8677fb5b905e7a8881caf91b20aa555f09d2c2a8
taiao <taiao.kawiti.wbhs@gmail.com>
2023-06-01 15:06:53 +1200

    Add particle animation to hero

--- portfolio-site/src/components/Hero.js
+++ portfolio-site/src/components/Hero.js
@@ -1,6 +1,9 @@
 import anime from 'animejs/lib/anime.es.js';
 import { useEffect } from 'react';
 import './Hero.css';
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
 const Hero = () => {
   useEffect(() => {
@@ -16,8 +19,81 @@ const Hero = () => {
     loadInAnimation();
   }, []);
 
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <div className='hero-section' id='hero'>
+      <Particles
+        id='tsparticles'
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fullScreen: false,
+          particles: {
+            number: { value: 30, density: { enable: true, value_area: 600 } },
+            color: { value: '#ffffff' },
+            shape: {
+              type: 'square',
+              stroke: { width: 0, color: '#000000' },
+              polygon: { nb_sides: 5 },
+            },
+            opacity: {
+              value: 0.1,
+              random: true,
+              anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+            },
+            size: {
+              value: 5,
+              random: true,
+              anim: { enable: false, speed: 2, size_min: 0.1, sync: false },
+            },
+            line_linked: {
+              enable: false,
+              distance: 300,
+              color: '#ffffff',
+              opacity: 0,
+              width: 0,
+            },
+            move: {
+              enable: true,
+              speed: 0.8,
+              direction: 'top',
+              straight: true,
+              out_mode: 'out',
+              bounce: false,
+              attract: { enable: false, rotateX: 600, rotateY: 1200 },
+            },
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: { enable: false, mode: 'repulse' },
+              onclick: { enable: false, mode: 'push' },
+              resize: true,
+            },
+            modes: {
+              grab: { distance: 800, line_linked: { opacity: 1 } },
+              bubble: {
+                distance: 790,
+                size: 79,
+                duration: 2,
+                opacity: 0.8,
+                speed: 3,
+              },
+              repulse: { distance: 400, duration: 0.4 },
+              push: { particles_nb: 4 },
+              remove: { particles_nb: 2 },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
       <h1 className='hero-name'>
         <span className='word1'>TAIAO</span>{' '}
         <span className='word2'>KAWITI</span>
