80dc3a5e5937e62026778b72690cf4203870d4e1
taiao <taiao.kawiti.wbhs@gmail.com>
2023-06-03 12:29:38 +1200

    Add animations to sections

--- portfolio-site/src/components/About.js
+++ portfolio-site/src/components/About.js
@@ -1,56 +1,65 @@
 import './About.css';
+import 'animate.css';
 import aboutPic from './img/about-pic.jpeg';
-import arrow from './img/down-arrow.svg';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
 // TODO: add github ticker "what ive been up to"
 export default function About() {
   return (
-    <div className='about-section' id='about'>
-      <h2 className='about-title'>ABOUT</h2>
-      <img src={aboutPic} alt='about' className='about-picture' />
-      <p className='about-me-blurb'>
-        Kia ora! My name is Taiao and I'm currently studying a diploma in web
-        development at Developers Institute NZ. I love to build beautiful and
-        functional websites and am looking for a full-stack development role.
-      </p>
-      <div className='tech-stack'>
-        <img
-          className='dev-icon html5'
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg'
-          alt='html5'
-        />
+    <AnimationOnScroll
+      animateIn='animate__fadeInLeft'
+      animateOnce={true}
+      animatePreScroll={false}
+      duration={1}
+    >
+      <div className='about-section' id='about'>
+        <h2 className='about-title'>ABOUT</h2>
+        <img src={aboutPic} alt='about' className='about-picture' />
+        <p className='about-me-blurb'>
+          Kia ora! My name is Taiao and I'm currently studying a diploma in web
+          development at Developers Institute NZ. I love to build beautiful and
+          functional websites and am looking for a full-stack development role.
+        </p>
+        <div className='tech-stack'>
+          <img
+            className='dev-icon html5'
+            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg'
+            alt='html5'
+          />
 
-        <img
-          className='dev-icon css3'
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg'
-          alt='css3'
-        />
+          <img
+            className='dev-icon css3'
+            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg'
+            alt='css3'
+          />
 
-        <img
-          className='dev-icon js'
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'
-          alt='js'
-        />
+          <img
+            className='dev-icon js'
+            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg'
+            alt='js'
+          />
 
-        <img
-          className='dev-icon react'
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'
-          alt='react'
-        />
+          <img
+            className='dev-icon react'
+            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'
+            alt='react'
+          />
 
-        <img
-          className='dev-icon nodejs'
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg'
-          alt='nodejs'
-        />
+          <img
+            className='dev-icon nodejs'
+            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg'
+            alt='nodejs'
+          />
 
-        <img
-          className='dev-icon docker'
-          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg'
-          alt='nodejs'
-        />
+          <img
+            className='dev-icon docker'
+            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg'
+            alt='nodejs'
+          />
+        </div>
+
+        <div className='divider'></div>
       </div>
-
-      <div className='divider'></div>
-    </div>
+    </AnimationOnScroll>
   );
 }
--- portfolio-site/src/components/Projects.js
+++ portfolio-site/src/components/Projects.js
@@ -1,32 +1,50 @@
 import ProjectCard from './Project';
 import './Projects.css';
+import 'animate.css';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 export default function Projects() {
   return (
     <div className='projects-section' id='projects'>
-      <h2 className='projects-title'>PROJECTS</h2>
-
+      <AnimationOnScroll
+        animateIn='animate__fadeIn'
+        animatePreScroll={false}
+        animateOnce={true}
+        duration={2}
+        offset={100}
+      >
+        <h2 className='projects-title'>PROJECTS</h2>
+      </AnimationOnScroll>
       <div className='projects-container'>
-        <ProjectCard
-          name='ONLINE SHOP'
-          image={require('./img/bionicboutique.png')}
-          description='An ecommerce site built with react and Stripe API'
-        />
-        <ProjectCard
-          name='CHATBOT'
-          image={require('./img/adventure-time-bmo.webp')}
-          description='yes'
-        />
-        <ProjectCard
-          name='PORTFOLIO SITE'
-          image={require('./img/personalwebsite.png')}
-          description='yes'
-        />
-        <ProjectCard
-          name='PLACEHOLDER PROJECT'
-          image={require('./img/personalwebsite.png')}
-          description='yes'
-        />
+        <AnimationOnScroll
+          animateIn='animate__fadeInLeft'
+          animatePreScroll={false}
+          animateOnce={true}
+          duration={1}
+        >
+          <ProjectCard
+            name='ONLINE SHOP'
+            image={require('./img/bionicboutique.png')}
+            description='An ecommerce site built with react and Stripe API'
+          />
+          <ProjectCard
+            name='CHATBOT'
+            image={require('./img/adventure-time-bmo.webp')}
+            description='yes'
+          />
+
+          <ProjectCard
+            name='PORTFOLIO SITE'
+            image={require('./img/personalwebsite.png')}
+            description='yes'
+          />
+
+          <ProjectCard
+            name='PLACEHOLDER PROJECT'
+            image={require('./img/personalwebsite.png')}
+            description='yes'
+          />
+        </AnimationOnScroll>
       </div>
 
       <div className='divider'></div>
