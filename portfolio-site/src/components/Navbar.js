7386b7c28112f494bc1c4ad5ce828454c3e66c1a
taiao <taiao.kawiti.wbhs@gmail.com>
2023-06-04 20:01:37 +1200

    Update CSS for navbar - media queries

--- portfolio-site/src/components/Navbar.js
+++ portfolio-site/src/components/Navbar.js
@@ -1,40 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './Navbar.css';
 
 import { motion } from 'framer-motion';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default function NavBar() {
   return (
-    <>
-      <Navbar inverse collapseOnSelect expand='sm' sticky='top'>
-        <motion.span whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
-          <Navbar.Brand href='#hero'>TAIAO KAWITI</Navbar.Brand>
-        </motion.span>
-        <Navbar.Toggle aria-controls='basic-navbar-nav'>
-          <Navbar.Collapse className='nav-container' id='basic-navbar-nav'>
-            <Nav>
-              <motion.span
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <Nav.Link href='#about'>ABOUT</Nav.Link>
-              </motion.span>
-              <motion.span
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <Nav.Link href='#projects'>PROJECTS</Nav.Link>
-              </motion.span>
-              <motion.span
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <Nav.Link href='#contact'>CONTACT</Nav.Link>
-              </motion.span>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar.Toggle>
-      </Navbar>
-    </>
+    <Navbar
+      className='m-auto'
+      inverse
+      collapseOnSelect
+      expand='lg'
+      sticky='top'
+      variant='dark'
+    >
+      <motion.span whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+        <Navbar.Brand className='mx-auto' href='#hero'>
+          TAIAO KAWITI
+        </Navbar.Brand>
+      </motion.span>
+      <Navbar.Toggle
+        className='navbar-hamburger'
+        aria-controls='basic-navbar-nav'
+      ></Navbar.Toggle>
+      <Navbar.Collapse className='nav-container' id='basic-navbar-nav'>
+        <Nav>
+          <motion.span whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+            <Nav.Link href='#about'>ABOUT</Nav.Link>
+          </motion.span>
+          <motion.span whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+            <Nav.Link href='#projects'>PROJECTS</Nav.Link>
+          </motion.span>
+          <motion.span whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+            <Nav.Link href='#contact'>CONTACT</Nav.Link>
+          </motion.span>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
